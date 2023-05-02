8684794ae2789ec347e1c87fd9d240ad22dd2be7
Nacu Florin <florin.nacu77@gmail.com>
2023-05-02 20:40:38 +0300

    more changes

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -3,11 +3,7 @@ import { Typography, Link } from "@material-ui/core";
 const Copyright = () => {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
-            {"Copyright © "}
-            <Link color="inherit" href="https://www.linkedin.com/in/florin-ionut-nacu-4770281b2/">
-                Nacu Florin Ionut
-            </Link>{" "}
-            {new Date().getFullYear()}
+            {"Copyright © "} {new Date().getFullYear()}
             {"."}
         </Typography>
     );
